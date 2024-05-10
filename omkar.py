20a56a362311ab06fec0ad7cd02a4062043dc743
obomble67 <86921970+obomble67@users.noreply.github.com>
2024-05-10 14:16:24 +0530

    Update omkar.py

--- omkar.py
+++ omkar.py
@@ -4,11 +4,9 @@
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
-# Convert Fahrenheit to Celsius
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
-# Example usage
 celsius_input = float(input("Enter temperature in Celsius: "))
 print("Temperature in Fahrenheit:", celsius_to_fahrenheit(celsius_input))
 
